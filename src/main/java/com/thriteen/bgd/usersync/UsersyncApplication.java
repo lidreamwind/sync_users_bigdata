4307edf1f3fc9890559d46af29809a9b9d10a767
DESKTOP-ENS3KU2\Administrator <15232560370@163.com>
2020-11-04 22:20:27 +0800

    构建项目

---
+++ src/main/java/com/thriteen/bgd/usersync/UsersyncApplication.java
@@ -0,0 +1,13 @@
+package com.thriteen.bgd.usersync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersyncApplication.class, args);
+	}
+
+}
