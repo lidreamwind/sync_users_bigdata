569e82e2988e33231b5ac4321b9c0959be105eaa
DESKTOP-ENS3KU2\Administrator <15232560370@163.com>
2020-11-19 17:33:56 +0800

    更新结构

--- src/main/java/com/thriteen/bgd/usersync/dao/dbmeta/UserLoginMapper.java
+++
@@ -1,8 +0,0 @@
-package com.thriteen.bgd.usersync.dao.dbmeta;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.thriteen.bgd.usersync.entity.po.UserLoginEntity;
-
-public interface UserLoginMapper extends BaseMapper<UserLoginEntity> {
-    public String selectByName();
-}
